ca5aad0003c5e1a63ea3e93b9bce6abaf669e6b2
Matthias <matthias0@skiff.com>
2023-03-27 13:41:45 +0200

    Add body styles

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-radix-gray-2 font-sans text-radix-gray-12 antialiased">
         <Main />
         <NextScript />
       </body>
