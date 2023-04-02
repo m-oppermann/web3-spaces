8a91e0ff4c1a4ae7ac9f6900b51484f8db4bc478
Matthias <matthias0@skiff.com>
2023-04-02 17:10:05 +0200

    Adjust account component

--- components/ui/Account.jsx
+++ components/ui/Account.jsx
@@ -4,9 +4,9 @@ export default function AccountComponent() {
   const ethAdress = "0x9a31E9e3D21602dEf187986Cc87a7daDc43D5434"
 
   return (
-    <div className="flex items-center gap-2 rounded-full bg-radix-gray-4 px-4 py-2 text-radix-gray-11">
+    <div className="pointer-events-none flex items-center gap-2 rounded-full bg-radix-gray-4 px-4 py-2 text-radix-gray-11">
       <motion.div
-        animate={{ opacity: [1, 0.6] }}
+        animate={{ opacity: [1, 0.75] }}
         transition={{
           duration: 1.5,
           repeat: Infinity,
