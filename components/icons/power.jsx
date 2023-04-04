04a155f0a33a5c9025145fa0bef2da9c52aaee95
Matthias <matthias0@skiff.com>
2023-04-04 15:27:13 +0200

    Fix attribute syntax of icon

--- components/icons/power.jsx
+++ components/icons/power.jsx
@@ -10,8 +10,8 @@ export default function PowerIconComponent({ className, ...props }) {
       {...props}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
       />
     </svg>
