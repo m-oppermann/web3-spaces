e2176c9dfe2fca7ac342b83f52545888b4d0dd5f
Matthias <matthias0@skiff.com>
2023-04-04 15:30:21 +0200

    Setup next-themes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css"
 import { Inter } from "next/font/google"
+import { ThemeProvider } from "next-themes"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,7 +17,13 @@ export default function App({ Component, pageProps }) {
           font-variant-ligatures: no-contextual;
         }
       `}</style>
-      <Component {...pageProps} />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
