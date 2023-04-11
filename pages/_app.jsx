6d5d7fc6158f0df1a79433c0de1b93ae6228e945
Matthias <matthias0@skiff.com>
2023-04-11 18:42:42 +0200

    Install and configure wagmi

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,36 @@ import "@/styles/globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "next-themes"
 
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi"
+import { publicProvider } from "wagmi/providers/public"
+
+import { MetaMaskConnector } from "wagmi/connectors/metaMask"
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: "swap",
+})
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [publicProvider()]
+)
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: "fd3c5f53911fb970a03a7fddd0ecf6ae",
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
 })
 
 export default function App({ Component, pageProps }) {
@@ -17,12 +43,11 @@ export default function App({ Component, pageProps }) {
           font-variant-ligatures: no-contextual;
         }
       `}</style>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-      >
-        <Component {...pageProps} />
+
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <WagmiConfig client={client}>
+          <Component {...pageProps} />
+        </WagmiConfig>
       </ThemeProvider>
     </>
   )
