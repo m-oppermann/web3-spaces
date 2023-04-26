96c99b4f87aed982846995e4deb60bf5938f6223
Matthias <matthias0@skiff.com>
2023-04-26 19:12:35 +0200

    Setup avatar and dialog component

---
+++ components/ui/Avatar.jsx
@@ -0,0 +1,58 @@
+import * as Avatar from "@radix-ui/react-avatar"
+import { forwardRef, useState } from "react"
+
+export default forwardRef(function AvatarComponent(
+  { ensAvatar, isLoadingAvatar, ...props },
+  ref
+) {
+  const handleKeyDown = event => {
+    if (event.key === "Enter") {
+      event.target.click()
+    }
+  }
+
+  return (
+    <Avatar.Root
+      onKeyDown={handleKeyDown}
+      className="h-10 w-10 cursor-pointer rounded-full focus:outline-none focus:ring-1 focus:ring-radix-gray-8 focus:ring-offset-2 focus:ring-offset-radix-gray-2 dark:focus:ring-radix-grayDark-8 dark:focus:ring-offset-radix-grayDark-2"
+      tabIndex={0}
+      role="button"
+      ref={ref}
+      {...props}
+    >
+      {isLoadingAvatar ? (
+        <div className="h-full w-full animate-pulse rounded-full bg-radix-gray-4 dark:bg-radix-grayDark-4" />
+      ) : (
+        <Avatar.Image
+          className="h-full w-full rounded-full"
+          src={ensAvatar}
+          alt="ENS Avatar"
+        />
+      )}
+      {ensAvatar === null && (
+        <Avatar.Fallback className="flex h-full w-full items-center justify-center rounded-full bg-radix-gray-4">
+          <svg
+            viewBox="0 0 100 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="50" cy="50" r="50" fill="url(#paint0_radial_101_3)" />
+            <defs>
+              <radialGradient
+                id="paint0_radial_101_3"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="rotate(90) scale(141.421)"
+              >
+                <stop offset="0.25" stopColor="#FBBF24" />
+                <stop offset="0.802083" stopColor="#D946EF" />
+              </radialGradient>
+            </defs>
+          </svg>
+        </Avatar.Fallback>
+      )}
+    </Avatar.Root>
+  )
+})
