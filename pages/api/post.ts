849590f6b2d907e72495e150863299ab2ba5951b
Matthias <matthias0@skiff.com>
2023-05-25 21:16:55 +0200

    Setup API routes to connect with database

---
+++ pages/api/post.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  const body = req.body
+  // Add post
+  if (req.method === "POST") {
+    try {
+      const newEntry = await prisma.post.create({
+        data: {
+          content: body.content,
+          user: { connect: { id: body.userId } },
+        },
+      })
+      return res.status(200).json(newEntry, { success: true })
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Error creating post", success: false })
+    }
+    // Read posts
+  } else if (req.method === "GET") {
+    try {
+      const posts = await prisma.post.findMany()
+      return res.status(200).json(posts, { success: true })
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Error reading posts", success: false })
+    }
+    // If method is not POST or GET
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false })
+  }
+}
---
+++ pages/api/user.ts
@@ -0,0 +1,56 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  const body = req.body
+  // Add user
+  if (req.method === "POST") {
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          address: body.address,
+          ensName: body.ensName,
+          ensAvatar: body.ensAvatar,
+        },
+      })
+      return res.status(200).json(newUser, { success: true })
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Error creating user", success: false })
+    }
+    // Update user
+  } else if (req.method === "PUT") {
+    try {
+      const updatedUser = await prisma.user.update({
+        where: {
+          address: body.address,
+        },
+        data: {
+          ensName: body.ensName,
+          ensAvatar: body.ensAvatar,
+        },
+      })
+      return res.status(200).json(updatedUser, { success: true })
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Error updating user", success: false })
+    }
+    // Read users
+  } else if (req.method === "GET") {
+    try {
+      const users = await prisma.user.findMany()
+      return res.status(200).json(users, { success: true })
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Error reading users", success: false })
+    }
+    // If method is not POST or GET
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false })
+  }
+}
