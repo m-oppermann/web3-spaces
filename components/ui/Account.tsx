d5581d6fe682c0032d11b988a2753727dda6696a
Matthias <matthias0@skiff.com>
2023-05-25 21:27:33 +0200

    Make it possible to add user records to database

--- components/ui/Account.tsx
+++ components/ui/Account.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext, use } from "react"
+import { UserContext } from "@/pages/index"
 
 import ConnectDropdown from "./ConnectDropdown"
 import Button from "./Button"
@@ -12,15 +13,69 @@ export default function AccountComponent() {
   const [mounted, setMounted] = useState(false)
 
   const { address, isConnected } = useAccount()
-  const { data: ensName } = useEnsName({ address })
+  const { data: ensName, isLoading: isLoadingName } = useEnsName({ address })
   const { data: ensAvatar, isLoading: isLoadingAvatar } = useEnsAvatar({
-    address,
+    name: ensName,
   })
   const { data: balance, isLoading: isLoadingBalance } = useBalance({ address })
+
+  const { users, isLoadingUsers } = useContext(UserContext)
 
   useEffect(() => {
     setMounted(true)
   }, [])
+
+  useEffect(() => {
+    if (
+      mounted &&
+      isConnected &&
+      !isLoadingName &&
+      !isLoadingAvatar &&
+      !isLoadingUsers &&
+      !users?.find(user => user.address === address)
+    ) {
+      handleNewUser()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    mounted,
+    isConnected,
+    address,
+    isLoadingName,
+    isLoadingAvatar,
+    isLoadingUsers,
+  ])
+
+  useEffect(() => {
+    if (
+      mounted &&
+      isConnected &&
+      !isLoadingUsers &&
+      !users?.find(user => user.ensName === ensName) &&
+      !users?.find(user => user.ensAvatar === ensAvatar)
+    ) {
+      handleUpdateUser()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mounted, isConnected, ensName, ensAvatar, isLoadingUsers])
+
+  const handleNewUser = async () => {
+    const body = { address, ensName, ensAvatar }
+    await fetch("/api/user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    })
+  }
+
+  const handleUpdateUser = async () => {
+    const body = { address, ensName, ensAvatar }
+    await fetch("/api/user", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    })
+  }
 
   if (!mounted) {
     return null
@@ -29,7 +84,7 @@ export default function AccountComponent() {
   if (isConnected) {
     return (
       <div className="flex gap-2">
-        <div className="pointer-events-none flex items-center gap-2.5 rounded-full bg-radix-gray-4 px-4 py-2 text-radix-gray-11 dark:bg-radix-grayDark-4 dark:text-radix-grayDark-11">
+        <div className="pointer-events-none flex items-center gap-2.5 rounded-full bg-radix-gray-4 px-4 py-2 text-radix-gray-11 dark:bg-radix-grayDark-4 dark:text-radix-grayDark-11 -sm:hidden">
           {ensName
             ? `${ensName}`
             : address.slice(0, 4) + "..." + address.slice(-4)}
@@ -39,15 +94,10 @@ export default function AccountComponent() {
             address={address}
             ensName={ensName}
             ensAvatar={ensAvatar}
-            isLoadingAvatar={isLoadingAvatar}
             balance={balance}
             isLoadingBalance={isLoadingBalance}
           >
-            <Avatar
-              address={address}
-              ensAvatar={ensAvatar}
-              isLoadingAvatar={isLoadingAvatar}
-            />
+            <Avatar address={address} ensAvatar={ensAvatar} />
           </AccountDialog>
         </Tooltip>
       </div>
