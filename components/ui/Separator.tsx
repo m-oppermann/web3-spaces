0f1782f13ffc4d28ea795ac116a45e4cb21b946a
Matthias <matthias0@skiff.com>
2023-05-25 21:31:49 +0200

    Adjust separator component

--- components/ui/Separator.tsx
+++ components/ui/Separator.tsx
@@ -1,9 +1,13 @@
 import * as Separator from "@radix-ui/react-separator"
+import clsx from "clsx"
 
-export default function SeparatorComponent() {
+export default function SeparatorComponent({ className }) {
   return (
     <Separator.Root
-      className="mx-1 h-7 w-px rounded bg-radix-gray-7 dark:bg-radix-grayDark-7 -sm:hidden"
+      className={clsx(
+        className,
+        "mx-1 w-px rounded bg-radix-gray-7 dark:bg-radix-grayDark-7 -sm:hidden"
+      )}
       orientation="vertical"
       decorative
     />
