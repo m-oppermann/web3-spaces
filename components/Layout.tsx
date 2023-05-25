1ea6967db758ba28c206f8946b241ae6215cb88f
Matthias <matthias0@skiff.com>
2023-05-25 21:09:52 +0200

    Refactor layout component and include head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,6 @@
+import Head from "next/head"
 import Header from "./Header"
+import Footer from "./Footer"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -7,8 +9,41 @@ interface LayoutProps {
 export default function LayoutComponent({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Web3:Spaces</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="A web3 way of exploring and sharing new ideas."
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          sizes="any"
+          href="/assets/favicon.svg"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="180x180"
+          href="/assets/apple-touch-icon.png"
+        />
+      </Head>
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   )
 }
