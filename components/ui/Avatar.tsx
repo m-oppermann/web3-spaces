d8b7f5c0b45ee93c6fbf8bfa658bb83910d6b577
Matthias <matthias0@skiff.com>
2023-05-25 21:29:05 +0200

    Refactor avatar component

--- components/ui/Avatar.tsx
+++ components/ui/Avatar.tsx
@@ -3,17 +3,17 @@ import { forwardRef, useState, useEffect } from "react"
 
 interface AvatarProps extends React.HTMLAttributes<HTMLSpanElement> {
   address: string
-  ensAvatar: string | null
-  isLoadingAvatar: boolean
+  ensAvatar?: string | null
 }
 
 export default forwardRef<HTMLSpanElement, AvatarProps>(
-  function AvatarComponent(
-    { address, ensAvatar, isLoadingAvatar, ...props },
-    ref
-  ) {
+  function AvatarComponent({ address, ensAvatar, ...props }, ref) {
     const [mounted, setMounted] = useState(false)
     const [colors, setColors] = useState([])
+
+    const gradient = {
+      background: `radial-gradient(100% 100% at 0% 0%, ${colors[0]} 35%, ${colors[1]} 65%, ${colors[2]} 100%)`,
+    }
 
     useEffect(() => {
       setMounted(true)
@@ -35,45 +35,23 @@ export default forwardRef<HTMLSpanElement, AvatarProps>(
         onKeyDown={event =>
           event.key === "Enter" && (event.target as HTMLDivElement).click()
         }
-        className="h-10 w-10 cursor-pointer rounded-full focus:outline-none focus:ring-1 focus:ring-radix-gray-8 focus:ring-offset-2 focus:ring-offset-radix-gray-2 dark:focus:ring-radix-grayDark-8 dark:focus:ring-offset-radix-grayDark-2"
+        className="relative h-10 w-10 cursor-pointer rounded-full focus:outline-none focus:ring-1 focus:ring-radix-gray-8 focus:ring-offset-2 focus:ring-offset-radix-gray-2 dark:focus:ring-radix-grayDark-8 dark:focus:ring-offset-radix-grayDark-2"
         tabIndex={0}
         role="button"
         ref={ref}
         {...props}
       >
-        {isLoadingAvatar ? (
-          <div className="h-full w-full animate-pulse rounded-full bg-radix-gray-4 dark:bg-radix-grayDark-4" />
-        ) : ensAvatar !== null ? (
+        <div className="h-full w-full">
           <Avatar.Image
-            className="h-full w-full rounded-full"
+            className="absolute h-full w-full rounded-full"
             src={ensAvatar}
             alt="ENS Avatar"
           />
-        ) : (
-          <Avatar.Fallback className="flex h-full w-full items-center justify-center rounded-full bg-radix-gray-4">
-            <svg
-              viewBox="0 0 100 100"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="50" cy="50" r="50" fill="url(#paint0_radial_110_2)" />
-              <defs>
-                <radialGradient
-                  id="paint0_radial_110_2"
-                  cx="0"
-                  cy="0"
-                  r="1"
-                  gradientUnits="userSpaceOnUse"
-                  gradientTransform="rotate(45) scale(141.421)"
-                >
-                  <stop offset="0.25" stopColor={colors[0]} />
-                  <stop offset="0.45" stopColor={colors[1]} />
-                  <stop offset="0.75" stopColor={colors[2]} />
-                </radialGradient>
-              </defs>
-            </svg>
-          </Avatar.Fallback>
-        )}
+          <Avatar.Fallback
+            style={gradient}
+            className="absolute h-full w-full rounded-full bg-radix-gray-4 dark:bg-radix-grayDark-4"
+          />
+        </div>
       </Avatar.Root>
     )
   }
