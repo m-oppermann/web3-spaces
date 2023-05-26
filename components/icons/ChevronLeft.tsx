701d1b1f5d4d288bae872984a395a40125b05d7a
Matthias <matthias0@skiff.com>
2023-05-26 17:40:45 +0200

    Add chevron icons as components

---
+++ components/icons/ChevronLeft.tsx
@@ -0,0 +1,19 @@
+export default function ChevronLeftComponent({ className, ...props }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 19.5L8.25 12l7.5-7.5"
+      />
+    </svg>
+  )
+}
