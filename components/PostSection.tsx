b2d4bd046f04eee11550fdac8f83737501ce5287
Matthias <matthias0@skiff.com>
2023-05-28 20:27:17 +0200

    Add loading animations

--- components/PostSection.tsx
+++ components/PostSection.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useScrollContainer } from "react-indiana-drag-scroll"
-import { motion } from "framer-motion"
+import { motion, useAnimate, stagger } from "framer-motion"
 import clsx from "clsx"
 
 import Post from "@/components/ui/Post"
@@ -12,10 +12,28 @@ export default function PostSectionComponent({
   currentSpace,
   currentUser,
   contributers,
+  isLoadingPosts,
 }) {
+  const [scope, animate] = useAnimate()
+
   const scrollContainer = useScrollContainer()
   const [scrollLeft, setScrollLeft] = useState(true)
   const [scrollRight, setscrollRight] = useState(false)
+
+  useEffect(() => {
+    if (!isLoadingPosts) {
+      animate(
+        "div[data-group]",
+        { opacity: [0, 1], y: [-20, 0] },
+        {
+          delay: stagger(0.15, { startDelay: 0.3 }),
+          type: "spring",
+          stiffness: 200,
+          damping: 20,
+        }
+      )
+    }
+  }, [animate, currentSpace, isLoadingPosts])
 
   const handleScrollEnd = event => {
     const { scrollLeft, scrollWidth, clientWidth } = event.target
@@ -25,50 +43,60 @@ export default function PostSectionComponent({
 
   return (
     <section className="relative mx-auto max-w-[1376px] lg:px-6 -lg:max-w-2xl -lg:p-6">
-      {posts?.length === 0 ? (
-        <div className="flex h-48 items-center justify-center rounded-2xl bg-radix-gray-4 dark:bg-radix-grayDark-4 lg:my-6 lg:h-64">
-          <span className="text-radix-gray-11 dark:text-radix-grayDark-11">
-            No posts yet.
-          </span>
-        </div>
+      {isLoadingPosts ? (
+        <div className="h-64 animate-pulse rounded-2xl bg-radix-gray-4 dark:bg-radix-grayDark-4 lg:my-6" />
       ) : (
         <>
-          <div
-            onScroll={handleScrollEnd}
-            className={clsx(
-              "flex gap-4 overflow-x-scroll lg:py-6 -lg:flex-col",
-              posts?.filter(post => post.spaceId === currentSpace?.id).length >
-                2 && "lg:cursor-grab lg:active:cursor-grabbing"
-            )}
-            ref={scrollContainer.ref}
-          >
-            {posts
-              ?.slice()
-              .reverse()
-              .filter(post => post.spaceId === currentSpace?.id)
-              .map((post, index) => (
-                <Post
-                  key={index}
-                  post={post}
-                  postNr={
+          {posts?.length === 0 ? (
+            <div className="flex h-64 items-center justify-center rounded-2xl bg-radix-gray-4 dark:bg-radix-grayDark-4 lg:mt-6">
+              <span className="text-radix-gray-11 dark:text-radix-grayDark-11">
+                No posts yet.
+              </span>
+            </div>
+          ) : (
+            <>
+              <div ref={scope}>
+                <div
+                  onScroll={handleScrollEnd}
+                  className={clsx(
+                    "flex min-h-[290px] gap-4 overflow-x-scroll lg:py-6 -lg:flex-col",
                     posts?.filter(post => post.spaceId === currentSpace?.id)
-                      .length - index
-                  }
-                  currentUser={currentUser}
-                  contributers={contributers}
-                  isConnected={isConnected}
-                  balance={balance}
-                />
-              ))}
-          </div>
-          <motion.div
-            animate={{ opacity: scrollLeft ? 0 : 1 }}
-            className="absolute left-6 top-6 bottom-6 w-12 bg-gradient-to-r from-radix-gray-2 to-transparent dark:from-radix-grayDark-2 -lg:hidden"
-          />
-          <motion.div
-            animate={{ opacity: scrollRight ? 0 : 1 }}
-            className="absolute right-6 top-6 bottom-6 w-12 bg-gradient-to-l from-radix-gray-2 to-transparent dark:from-radix-grayDark-2 -lg:hidden"
-          />
+                      .length > 2 && "lg:cursor-grab lg:active:cursor-grabbing"
+                  )}
+                  ref={scrollContainer.ref}
+                >
+                  {posts
+                    ?.slice()
+                    .reverse()
+                    .filter(post => post.spaceId === currentSpace?.id)
+                    .map((post, index) => (
+                      <div data-group key={index}>
+                        <Post
+                          post={post}
+                          postNr={
+                            posts?.filter(
+                              post => post.spaceId === currentSpace?.id
+                            ).length - index
+                          }
+                          currentUser={currentUser}
+                          contributers={contributers}
+                          isConnected={isConnected}
+                          balance={balance}
+                        />
+                      </div>
+                    ))}
+                </div>
+              </div>
+              <motion.div
+                animate={{ opacity: scrollLeft ? 0 : 1 }}
+                className="absolute left-6 top-6 bottom-6 w-12 bg-gradient-to-r from-radix-gray-2 to-transparent dark:from-radix-grayDark-2 -lg:hidden"
+              />
+              <motion.div
+                animate={{ opacity: scrollRight ? 0 : 1 }}
+                className="absolute right-6 top-6 bottom-6 w-12 bg-gradient-to-l from-radix-gray-2 to-transparent dark:from-radix-grayDark-2 -lg:hidden"
+              />
+            </>
+          )}
         </>
       )}
     </section>
--- components/ui/SpaceGroup.tsx
+++ components/ui/SpaceGroup.tsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react"
+import { useAnimate, stagger } from "framer-motion"
+
 import ChevronLeft from "@/components/icons/ChevronLeft"
 import ChevronRight from "@/components/icons/ChevronRight"
 
@@ -10,79 +13,110 @@ export default function SpaceGroupComponent({
   currentSpaceNr,
   setCurrentSpaceNr,
   contributers,
+  isLoadingSpaces,
   isLoadingPosts,
 }) {
+  const [scope, animate] = useAnimate()
+
+  useEffect(() => {
+    if (!isLoadingPosts) {
+      animate("div[data-item]", { opacity: [0, 1] }, { delay: 0.15 })
+    }
+  }, [animate, isLoadingPosts])
+
+  useEffect(() => {
+    if (!isLoadingSpaces) {
+      animate(
+        "div[data-group]",
+        { opacity: [0, 1] },
+        { delay: stagger(0.15, { startDelay: 0.3 }), duration: 0.5 }
+      )
+    }
+  }, [animate, currentSpace, isLoadingSpaces])
+
   return (
     <div className="w-full flex-grow">
-      <div className="mb-3 flex items-center gap-2">
-        <div className="flex items-center gap-0.5 rounded-full bg-radix-gray-4 p-1.5 dark:bg-radix-grayDark-4">
-          <button
-            onClick={() => setCurrentSpaceNr(currentSpaceNr - 1)}
-            className="group focus:outline-none disabled:opacity-50"
-            disabled={currentSpaceNr <= 1}
-          >
-            <ChevronLeft
-              className="stroke-radix-gray-11 group-focus:stroke-radix-gray-12 dark:stroke-radix-grayDark-11 dark:group-focus:stroke-radix-grayDark-12"
-              height={18}
-            />
-          </button>
-          <span className="pointer-events-none text-sm font-medium">
-            {currentSpace?.id + " "}
-            <span className="text-radix-gray-9 dark:text-radix-grayDark-9">
-              /
-            </span>
-            {" " + spaces?.length}
-          </span>
-          <button
-            onClick={() => setCurrentSpaceNr(currentSpaceNr + 1)}
-            className="group focus:outline-none disabled:opacity-50"
-            disabled={currentSpaceNr >= spaces?.length}
-          >
-            <ChevronRight
-              className="stroke-radix-gray-11 group-focus:stroke-radix-gray-12 dark:stroke-radix-grayDark-11 dark:group-focus:stroke-radix-grayDark-12"
-              height={18}
-            />
-          </button>
-        </div>
-      </div>
-      <h1 className="mb-4 w-11/12 text-4xl font-medium -sm:text-3xl -xs:text-2xl">
-        {currentSpace?.title}
-      </h1>
-      <p className="mb-8 text-xl text-radix-gray-11 dark:text-radix-grayDark-11 -lg:mb-6 -sm:text-lg">
-        {currentSpace?.description}
-      </p>
-      <div className="flex h-10 items-center gap-3">
-        {posts?.length !== 0 && (
-          <ul className="ml-3 flex items-center">
-            {contributers?.slice(0, 3).map((contributer, index) => (
-              <li
-                key={index}
-                className="pointer-events-none -ml-3 flex rounded-full border-2 border-radix-gray-2 dark:border-radix-grayDark-2"
-                style={{ zIndex: index }}
+      {isLoadingSpaces ? (
+        <>
+          <div className="mb-3 h-8 w-20 animate-pulse rounded-full bg-radix-gray-4 p-1.5 dark:bg-radix-grayDark-4" />
+          <div className="mb-8 h-[180px] w-full animate-pulse rounded-2xl bg-radix-gray-4 p-1.5 dark:bg-radix-grayDark-4" />
+          <div className="h-10 w-48 animate-pulse rounded-xl bg-radix-gray-4 p-1.5 dark:bg-radix-grayDark-4" />
+        </>
+      ) : (
+        <div ref={scope}>
+          <div data-item className="mb-3 flex items-center gap-2">
+            <div className="flex items-center gap-0.5 rounded-full bg-radix-gray-4 p-1.5 dark:bg-radix-grayDark-4">
+              <button
+                onClick={() => setCurrentSpaceNr(currentSpaceNr - 1)}
+                className="group focus:outline-none disabled:opacity-50"
+                disabled={currentSpaceNr <= 1}
               >
-                <Avatar
-                  address={contributer.address}
-                  ensAvatar={contributer.ensAvatar}
-                  tabIndex={-1}
+                <ChevronLeft
+                  className="stroke-radix-gray-11 group-focus:stroke-radix-gray-12 dark:stroke-radix-grayDark-11 dark:group-focus:stroke-radix-grayDark-12"
+                  height={18}
                 />
-              </li>
-            ))}
-            {contributers?.length > 3 && (
-              <li className="dark:bg-radiy-grayDark-4 pointer-events-none z-[3] -ml-3 box-content flex h-10 w-10 items-center justify-center rounded-full border-2 border-radix-gray-2 bg-radix-gray-4 text-radix-gray-11 dark:border-radix-grayDark-2 dark:bg-radix-grayDark-4 dark:text-radix-grayDark-11 ">
-                <span className="-mr-0.5">
-                  {contributers?.length - 3 + "+"}
+              </button>
+              <span className="pointer-events-none text-sm font-medium">
+                {currentSpace?.id + " "}
+                <span className="text-radix-gray-9 dark:text-radix-grayDark-9">
+                  /
                 </span>
-              </li>
-            )}
-          </ul>
-        )}
-        {!isLoadingPosts && (
-          <div className="flex gap-1 text-radix-gray-11 dark:text-radix-grayDark-11">
-            <span className="min-w-[10px]">{contributers?.length}</span>
-            <span>Contributor{contributers?.length !== 1 && "s"}</span>
+                {" " + spaces?.length}
+              </span>
+              <button
+                onClick={() => setCurrentSpaceNr(currentSpaceNr + 1)}
+                className="group focus:outline-none disabled:opacity-50"
+                disabled={currentSpaceNr >= spaces?.length}
+              >
+                <ChevronRight
+                  className="stroke-radix-gray-11 group-focus:stroke-radix-gray-12 dark:stroke-radix-grayDark-11 dark:group-focus:stroke-radix-grayDark-12"
+                  height={18}
+                />
+              </button>
+            </div>
           </div>
-        )}
-      </div>
+          <div data-group>
+            <h1 className="mb-4 w-11/12 text-4xl font-medium -sm:text-3xl -xs:text-2xl">
+              {currentSpace?.title}
+            </h1>
+            <p className="mb-8 text-xl text-radix-gray-11 dark:text-radix-grayDark-11 -lg:mb-6 -sm:text-lg">
+              {currentSpace?.description}
+            </p>
+          </div>
+          <div data-group="1" className="flex h-10 items-center gap-3">
+            {posts?.length !== 0 && (
+              <ul className="ml-3 flex items-center">
+                {contributers?.slice(0, 3).map((contributer, index) => (
+                  <li
+                    key={index}
+                    className="pointer-events-none -ml-3 flex rounded-full border-2 border-radix-gray-2 dark:border-radix-grayDark-2"
+                    style={{ zIndex: index }}
+                  >
+                    <Avatar
+                      address={contributer.address}
+                      ensAvatar={contributer.ensAvatar}
+                      tabIndex={-1}
+                    />
+                  </li>
+                ))}
+                {contributers?.length > 3 && (
+                  <li className="dark:bg-radiy-grayDark-4 pointer-events-none z-[3] -ml-3 box-content flex h-10 w-10 items-center justify-center rounded-full border-2 border-radix-gray-2 bg-radix-gray-4 text-radix-gray-11 dark:border-radix-grayDark-2 dark:bg-radix-grayDark-4 dark:text-radix-grayDark-11 ">
+                    <span className="-mr-0.5">
+                      {contributers?.length - 3 + "+"}
+                    </span>
+                  </li>
+                )}
+              </ul>
+            )}
+            {!isLoadingPosts && (
+              <div className="flex gap-1 text-radix-gray-11 dark:text-radix-grayDark-11">
+                <span className="min-w-[10px]">{contributers?.length}</span>
+                <span>Contributor{contributers?.length !== 1 && "s"}</span>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
