b0db9fe67f7ac021dbf7b93fb3034b7464fdd756
Matthias <matthias0@skiff.com>
2023-06-01 19:15:23 +0200

    Refactor prisma import

--- pages/api/space.ts
+++ pages/api/space.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
+import prisma from "@/lib/prisma"
 
 export default async function handler(req, res) {
   const body = req.body
