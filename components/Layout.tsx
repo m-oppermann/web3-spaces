165ffa0ae38357d5901e8e35526523818961f8a1
Matthias <matthias0@skiff.com>
2023-06-02 16:48:22 +0200

    Edit meta data, add open graph and update icon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,12 +10,37 @@ export default function LayoutComponent({ children }: LayoutProps) {
   return (
     <>
       <Head>
+        {/* Primary Meta Tags */}
         <title>Web3:Spaces</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="A web3 way of exploring and sharing new ideas."
+          content="A web3 app to share thoughts and explore new ideas."
         />
+        {/* Open Graph / Facebook */}
+        <meta property="og:url" content="https://web3-spaces.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Web3:Spaces" />
+        <meta
+          property="og:description"
+          content="A web3 app to share thoughts and explore new ideas."
+        />
+        <meta
+          property="og:image"
+          content="https://web3-spaces.vercel.app/assets/preview.jpg"
+        />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Web3:Spaces" />
+        <meta
+          property="twitter:description"
+          content="A web3 app to share thoughts and explore new ideas."
+        />
+        <meta
+          property="twitter:image"
+          content="https://web3-spaces.vercel.app/assets/preview.jpg"
+        />
+        {/* Favicon */}
         <link
           rel="icon"
           type="image/svg+xml"
@@ -34,6 +59,7 @@ export default function LayoutComponent({ children }: LayoutProps) {
           sizes="32x32"
           href="/assets/favicon-32x32.png"
         />
+        {/* Apple Touch Icon */}
         <link
           rel="apple-touch-icon"
           type="image/png"
